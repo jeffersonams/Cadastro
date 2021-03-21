271852198242a5091b920621d0075a38f52f7758
Jefferson <jefferson.amsantos@hotmail.com>
2021-03-21 13:41:16 -0300

    Projeto pronto

---
+++ Cadastro/Clientes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadastro
+{
+    class Clientes
+    {
+        // ATRIBUTOS DA CLASSE CLIENTES
+        string Nome;
+        string Sobremenome;
+        int Cpf;
+        string Rua;
+        string Bairro;
+        int Numero;
+
+
+        //CONTRUTOR COM PARAMETRO
+        public Clientes(string nome, string sobrenome, int CPF, string rua, string bairro, int numero)
+        {
+            Nome = nome;
+            Sobremenome = sobrenome;
+            Cpf = CPF;
+            Rua = rua;
+            Bairro = bairro;
+            Numero = numero;
+                                 
+        }
+
+
+
+
+
+        //CRIANDO UM METODO PARA LER
+        public override string ToString()
+        {
+            return "CADASTRO REALIZADO COM SUCESSO"
+                + Nome
+                + ", "
+                + Sobremenome
+                + ", "
+                + Cpf
+                + ", "
+                + Rua
+                + ", "
+                + Bairro
+                + ", "
+                + Numero;
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Cadastro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Cadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("BEM VINDO AO SISTEMA DE CADASTRO");
+
+            Console.Write("Nome do paciente: ");
+            string nome = Console.ReadLine(); //VARIAVEIS AUXILIARES PARA COLOCAR DENTRO DO OBJETO
+
+            Console.Write("Sobrenome do paciente: ");
+            string sobrenome = Console.ReadLine();  //VARIAVEIS AUXILIARES PARA COLOCAR DENTRO DO OBJETOv
+
+            Console.Write("CPF: ");
+            int cpf = int.Parse(Console.ReadLine());
+
+            Console.Write("Rua: ");
+            string rua = Console.ReadLine();
+
+            Console.Write("Bairro: ");
+            string bairro = Console.ReadLine();
+
+            Console.Write("Numero: ");
+            int  numero = int.Parse (Console.ReadLine());
+
+
+            //CRIANDO UM OBJETO DA CLASSE CLIENTES
+            Clientes cli = new Clientes(nome, sobrenome, cpf, rua, bairro, numero);
+
+            Console.WriteLine(cli);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
